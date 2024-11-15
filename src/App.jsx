6a57da1d8eadcc46964410757c8fd9fbb7419584
Long Nguyen <barnonguyen@gmail.com>
2024-11-15 18:26:40 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import './App.css';
 const apiKey = import.meta.env.VITE_URL; // Replace with your actual API key
 const location = 'Berlin'; // Specify the location you want
 const dayForecast = 5;
-const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=${dayForecast}&aqi=yes&alerts=yes`;
+const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=${dayForecast}&aqi=yes&alerts=no`;
+
+
 
 function App() {
-  const [info, setInfo] = useState(null);
-  const [location, setLocation] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [locations, setLocations] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,8 +20,10 @@ function App() {
         
         const data = await result.json(); // Await JSON parsing
         console.log(data);
-        setInfo(data.current);
-        setLocation(data.location)
+        setCurrentWeather(data.current);
+        setLocations(data.location);
+        setForecast(data.forecast);
+        
 
       } catch (error) {
         console.log('error:', error);
@@ -28,12 +33,46 @@ function App() {
     fetchData();
   }, []);
 
+
+
   return (
     <div className="body-container">
       
       <div className="weather-container">
 
-        
+        <div className="leftside">
+
+          <div className="search-bar">
+            <input type="text" placeholder='City' />
+          </div>
+
+          <div className="current-weather">
+
+            <div className="temp">
+
+              <h1>{currentWeather.temp_c}</h1>
+              <img src={currentWeather.condition.icon} alt="" />
+
+            </div>
+
+            <div className="location">
+              <h1>{locations.name}</h1>
+            </div>
+
+            <div className="time">
+
+              <h2>{locations.localtime}</h2>
+              <hr />
+              <h2>H:{forecast}</h2>           
+              <h2>L:{todayMin}</h2>           
+            
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className="rightside"></div>
 
       </div>
       
